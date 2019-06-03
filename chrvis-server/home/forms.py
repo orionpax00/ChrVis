724fa07637d6866393a94e90506b1ff2937fbb7e
orionpax00 <durgesh123.iitr@gmail.com>
2019-06-04 08:01:44 +0900

    dockerization started

--- chrvis-server/home/forms.py
+++ chrvis-server/home/forms.py
@@ -4,4 +4,4 @@ from .models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('description', 'document','email' )
+        fields = ('description', 'matrix_file','annotation_file','email' )
--- chrvis-server/home/models.py
+++ chrvis-server/home/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 # Create your models here.
 
-def user_directory_path(instance, filename):
-    return ('{0}/{1}'.format(instance.email.split('@')[0], filename))
+def user_directory_path_matrix(instance, filename):
+    return ('{0}/{1}.matrix'.format(instance.email.split('@')[0], filename))
+
+def user_directory_path_annot(instance, filename):
+    return ('{0}/{1}.annot'.format(instance.email.split('@')[0], filename))
 
 
 class Document(models.Model):
     description = models.CharField(max_length=255, blank=True)
     email = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to=user_directory_path)
+    matrix_file = models.FileField(upload_to=user_directory_path_matrix)
+    annotation_file = models.FileField(upload_to=user_directory_path_annot)
     uploaded_at = models.DateTimeField(auto_now_add=True)
