0159d60b1681e5d7b1509c990a6685c22be107f4
orionpax00 <durgesh123.iitr@gmail.com>
2019-07-05 19:25:52 +0900

    More network Stuff

--- scripts/wt.py
+++ scripts/wt.py
@@ -2,16 +2,20 @@ import numpy as np
 import pywt
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+#
 
-df = pd.read_csv("mprobc_chr1_100kb.csv",delimiter=',', header=None)
+df = pd.read_csv("../../mprobc_250kb.csv",delimiter=',', header=None)
+
 
 header = df.pop(0)
-df.head()
+scaler = MinMaxScaler()
+scaled_values_t = scaler.fit_transform(df)
 
-coeffs2 = pywt.dwt2(df.values, 'bior1.3')
+# coeffs2 = pywt.dwt2(scaled_values_t, 'bior1.3')
 
-print(coeffs2[0])
-np.save('wt',np.absolute(coeffs2[0]))
+# print(coeffs2[0])
+# np.save('wt',np.absolute(coeffs2[0]))
 #
-plt.imshow(coeffs2[0])
+plt.imshow(scaled_values_t)
 plt.show()
